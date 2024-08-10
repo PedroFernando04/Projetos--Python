6688e5b14e84569a42021e0b1a3e357d3f32706e
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-10 20:54:25 -0300

    Add files via upload

---
+++ Lista 01 - Lab. progamação 2/2.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça dois números e imprima o maior deles
+
+numero1 = int(input("Informe seu primeiro número: "))
+numero2 = int(input("Informe seu segundo número: "))
+
+if numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2)
