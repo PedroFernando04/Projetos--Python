f95163cdc6a4acca132b0c8d6dd0de81c5419b22
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-10 20:56:53 -0300

    Delete Lista 01 - Lab. progamação 2 directory

--- Lista 01 - Lab. progamação 2/1.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que peça um número inteiro e determine se ele par ou ímpar. Dica: Utilize o operador módulo(resto de divisão[%])
-
-numero = int(input("Digite um número inteiro: "))
-
-if numero % 2 == 0:
-    print("É par!")
-else:
-    print("É ímpar!")
--- Lista 01 - Lab. progamação 2/3.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que verifique se uma letra digitada é vogal ou consoante
-
-letra = input("Digite uma letra: ")
-
-if letra == ('a' or 'e' or 'i' or 'o' or 'u'):
-    print("É uma vogal")
-else:
-    print("É uma consoante")
