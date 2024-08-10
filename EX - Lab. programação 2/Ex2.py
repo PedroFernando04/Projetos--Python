15b641cd4a84b9036f2c8f62c22f3bb9331bdb50
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-10 20:56:45 -0300

    Delete EX - Lab. programação 2 directory

--- EX - Lab. programação 2/Ex2.py
+++
@@ -1,8 +0,0 @@
-valor_produto = float(input("Informe o valor do seu produto: "))
-
-desconto = valor_produto * 15/100
-
-valor_com_desconto = valor_produto - desconto
-
-print("Valor total: {:.2f}".format(valor_com_desconto))
-print("(Valor integral: {:.2f} - Desconto: {:.2f})".format(valor_produto, desconto))
