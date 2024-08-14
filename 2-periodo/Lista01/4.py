efdae0a8ee1a9cfbe45e7d45b9f44891caabb7a0
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-14 08:44:00 -0300

    Update 4.py

--- 2-periodo/Lista01/4.py
+++ 2-periodo/Lista01/4.py
@@ -4,3 +4,18 @@ Faça um programa para a leitura de duas notas parciais de um aluno. O programa 
     b. A mensagem "Reprovado", se a média for menor do que sete;
     c. A mensagem "Aprovado com Distinção", se a média for igual a 10.
 '''
+
+notas_somadas = 0
+
+for i in range(1, 3):
+    nota = float(input(f"Insira sua {i}° nota: "))
+    notas_somadas += nota
+
+media = notas_somadas/2
+
+if media == 10:
+    print("Aprovado com Distinção")
+elif media >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
