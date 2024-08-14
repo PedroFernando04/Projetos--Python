47f46410e509156f5d48bd513852a153ff324ec3
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-14 08:44:50 -0300

    Update 5.py

--- 2-periodo/Lista01/5.py
+++ 2-periodo/Lista01/5.py
@@ -1,1 +1,10 @@
 #Faça um programa que leia três números e mostre o maior deles
+
+auxiliar = 0
+
+for i in range(1,4):
+    numero = float(input(f"Informe o {i}° número: "))
+    if numero >= auxiliar:
+        auxiliar = numero
+print(auxiliar)
+    
