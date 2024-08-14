f416ac041ee58f56e7811de5d53f0842e8f7e739
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-14 08:45:44 -0300

    Update 6.py

--- 2-periodo/Lista01/6.py
+++ 2-periodo/Lista01/6.py
@@ -2,3 +2,15 @@
 Faça um programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-vespertino ou N-noturno.
 Imprima a mensagem "Bom dia!", "Boa tarde!" ou "Boa noite!" ou "Valor inválido!", conforme o caso.
 '''
+
+turno = input("Informe o horário que você estuda: ").lower()
+
+match turno:
+    case 'm':
+        print("Bom dia!")
+    case 'v':
+        print("Boa tarde!")
+    case 'n':
+        print("Boa noite!")
+    case _:
+        print("Valor inválido!")
