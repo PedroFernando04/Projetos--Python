56c84397909a420dca007a8285f300f92d792cf2
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-26 16:34:07 -0300

    Update and rename alo to 4-incompleto.py

---
+++ 2-periodo/Lista02/4-incompleto.py
@@ -0,0 +1,11 @@
+'''
+ Faça um programa que leia e valide as seguintes informações:
+    1. Nome: maior que 3 caracteres;
+    2. Idade: entre 0 e 150;
+    3. Salário: maior que zero;
+    4. Sexo: 'f' ou 'm';
+    5. Estado Civil: 's', 'c', 'v', 'd';
+    5. Use a função len(string) para saber o tamanho de um texto(número de caracteres)
+ '''
+
+g
