88a85ee4be34ec16195cc6a5ffd44cafaa6e9ff0
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-20 08:43:30 -0300

    Update Exercicio01.py

--- 2-periodo/AV02/Exercicio01.py
+++ 2-periodo/AV02/Exercicio01.py
@@ -1,24 +1,35 @@
-print("Bem vindo(a) a Calculadora!\n\nSelecione a operção que deseja realizar:")
-print("\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n")
-
 op = [["Adição", '+'], ["Subtração", '-'], ["Multiplicação", 'x'], ["Divisão", '%']]
 
-operacao = int(input())
+print("Bem vindo(a) a Calculadora!\n")
 
-print(f"\nVocê selecionou - {op[operacao - 1][0]}\n")
-print("Agora informe os números que serão utilizados nessa operação:")
+while 1:
+    print("\nSelecione a operção que deseja realizar:\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n")
 
-n1 = int(input("\nInforme o primeiro número: "))
-n2 = int(input("Informe o segundo número: "))
+    operacao = int(input())
 
-match(operacao):
-    case 1:
-        resultado = n1 + n2
-    case 2: 
-        resultado = n1 - n2
-    case 3:
-        resultado = n1 * n2
-    case 4:
-        resultado = n1 / n2
+    print(f"\nVocê selecionou - {op[operacao - 1][0]}\n")
+    print("Agora informe os números que serão utilizados nessa operação:")
 
-print(f"\n{n1} {op[operacao - 1][1]} {n2} = {resultado}")
+    n1 = float(input("\nInforme o primeiro número: "))
+    n2 = float(input("Informe o segundo número: "))
+
+    match(operacao):
+        case 1:
+            resultado = n1 + n2
+        case 2: 
+            resultado = n1 - n2
+        case 3:
+            resultado = n1 * n2
+        case 4:
+            while(n2 == 0):
+                print("\nValor inválido!\nO denominador não pode ser 0!\n")
+                n2 = float(input("Informe o segundo número: "))
+
+            resultado = n1 / n2
+
+    print(f"\n{n1:.2f} {op[operacao - 1][1]} {n2:.2f} = {resultado:.2f}\n")
+
+    loop = input("Deseja fazer uma nova operação?[S/N]").lower()
+    if loop == 'n':
+        print("Encerrando...")
+        break
